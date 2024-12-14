11b0f7f23811c3702d5fba8c54c3fc2005662364
Susantha Perera <33259705+mskperera@users.noreply.github.com>
2024-12-14 11:54:02 +0530

    uploading files into a specified folder path
    modified the file storage system to facilitate uploading files into a specified folder path, enabling users to upload files directly into the desired folder

--- utils/fileService.js
+++ utils/fileService.js
@@ -2,17 +2,18 @@
 const pool = require('../mysql/models/imageModel');
 
 // Function to save file information into the database
-const saveFileInfo = async (fileHash, fileName, filePath, isResized = false) => {
+const saveFileInfo = async (fileHash, fileName, filePath, folderPath, isResized = false) => {
   try {
     const [result] = await pool.query(
-      'INSERT INTO images (hash, file_name, file_path, is_resized) VALUES (?, ?, ?, ?)',
-      [fileHash, fileName, filePath, isResized]
+      'INSERT INTO images (hash, file_name, file_path, folder_path, is_resized) VALUES (?, ?, ?, ?, ?)',
+      [fileHash, fileName, filePath, folderPath, isResized]
     );
-    return result; // You can return the result if needed (e.g., inserted ID)
+    return result;
   } catch (err) {
     throw new Error('Error saving file info to database: ' + err.message);
   }
 };
+
 
 module.exports = {
   saveFileInfo,
